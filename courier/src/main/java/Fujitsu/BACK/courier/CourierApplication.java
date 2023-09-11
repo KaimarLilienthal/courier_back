4400116c873c94fedc542ba5fda5de23d9a267c5
Your GitHub Username <kaimarlilienthal@gmail.coml>
2023-09-11 22:48:14 +0300

    Initial commit

---
+++ courier/src/main/java/Fujitsu/BACK/courier/CourierApplication.java
@@ -0,0 +1,13 @@
+package Fujitsu.BACK.courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierApplication.class, args);
+	}
+
+}
---
+++ courier/src/test/java/Fujitsu/BACK/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package Fujitsu.BACK.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
