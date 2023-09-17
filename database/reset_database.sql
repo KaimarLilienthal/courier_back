8080737f059851c3b2786f20cdd54aba634a302e
Your GitHub Username <kaimarlilienthal@gmail.coml>
2023-09-17 12:28:38 +0300

    WP3

--- database/reset_database.sql
+++ database/reset_database.sql
@@ -1,18 +1,6 @@
--- postgres
--- Kustutab public schema (mis põhimõtteliselt kustutab kõik tabelid)
-DROP SCHEMA public CASCADE;
--- Loob uue public schema vajalikud õigused
-CREATE SCHEMA public
--- taastab vajalikud andmebaasi õigused
-    GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
 
 -- H2
--- Kustutab public schema (mis põhimõtteliselt kustutab kõik tabelid)
-DROP SCHEMA public CASCADE;
--- Loob uue public schema vajalikud õigused
-CREATE SCHEMA public
--- taastab vajalikud andmebaasi õigused
-    GRANT ALL ON SCHEMA public TO H2;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+-- Deletes the public schema (which essentially deletes all tables)
+DROP ALL OBJECTS DELETE FILES;
+-- Restores necessary database privileges
+GRANT ALL ON SCHEMA PUBLIC TO SA;
